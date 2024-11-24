b5925055df568f527cba8cb95c3d9b31fb26468c
perman03 <frankperman03@gmail.com>
2024-11-24 15:00:48 -0600

    fix name details

--- features/step-definitions/cotizacion.steps.js
+++ features/step-definitions/cotizacion.steps.js
@@ -89,7 +89,7 @@ When('I view my car info and extract it', async()=>{
 
     
     const csvWriter = createObjectCsvWriter({
-        path: 'banorteInfo.csv',
+        path: 'BanorteInfo.csv',
         header: [
             {id: 'carInfo', title: 'Car Info'},
             {id: 'carYear', title: 'Car Year'},
